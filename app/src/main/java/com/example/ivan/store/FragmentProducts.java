7ffa0b0e80492709a362bd5980bb8a8f15afc49b
Ivan <ivanmamedov21@gmail.com>
2017-10-16 22:01:02 +0500

    Hello

---
+++ app/src/main/java/com/example/ivan/store/FragmentProducts.java
@@ -0,0 +1,39 @@
+package com.example.ivan.store;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentProducts extends Fragment  {
+
+    public FragmentProducts() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_products, container, false);
+
+        return view;
+    }
+
+
+}
